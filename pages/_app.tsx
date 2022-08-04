6b95b106436dff3439664207ec121521deacd612
Kakha D Mikeladze <workingkakha@Kakhas-MacBook-Pro.local>
2022-08-04 13:06:10 +0400

    routing and theme done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,9 @@
 import '../styles/globals.css'
+import '../public/assets/css/plugins.css'
+import '../public/assets/css/style.css'
+// import '../public/assets/css/style.css.map'
+
+
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
