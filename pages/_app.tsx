97c40389b4fac358c7756e619108ae75e98f4d0b
Kakha D Mikeladze <workingkakha@Kakhas-MacBook-Pro.local>
2022-08-10 20:43:48 +0400

    redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,15 @@ import '../public/assets/css/plugins.css'
 import '../public/assets/css/style.css'
 import '../public/assets/css/colors/red.css'
 import '../public/assets/fonts/dm.css'
-
+import store from '../redux/store'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import Script from 'next/script'
-
+import { Provider} from 'react-redux'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+
+    <Provider store={store}>
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -19,7 +20,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Component {...pageProps} />
       <Script src="/assets/js/plugins.js" strategy="afterInteractive"></Script>
       <Script src="/assets/js/theme.js" strategy="lazyOnload"></Script>
-    </>
+    </Provider>
   );
 }
  
