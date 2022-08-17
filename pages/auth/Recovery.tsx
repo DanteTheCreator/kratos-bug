af2563992e66145e1f70538c381ffaeae2fc7e46
nika-ivanidze <Nika01@Nikas-MacBook-Pro.local>
2022-08-17 17:14:48 +0400

    #4 recovery page redesign

--- pages/auth/Recovery.tsx
+++ pages/auth/Recovery.tsx
@@ -110,79 +110,68 @@ const Recovery = () => {
   };
 
   return (
-    <div>
-      <div className="container row">
-        <div
-          className="col-10 ps-0 offset-3"
-          style={{
-            width: "250px",
-            height: "50px",
-            position: "relative",
-            top: "60px",
-          }}
-        >
-          <Image
-            className={`${styles.company_logo}`}
-            src={companyLogo}
-            alt="image"
-            layout="fill"
-          />
-        </div>
-      </div>{" "}
-      <div className={styles.container}>
-        <div className="container d-flex flex-column align-items-center ms-0 px-0">
-          <h4 className="col-8 px-0 ms-0">
-            <b>Reset Your Password</b>
-            <p className={`${styles.header_text} pt-2 mb-4`}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe at
-              veniam autem soluta reiciendis repellat.
-            </p>
-          </h4>
-          <div className="form-floating mb-4 col-8 px-0">
-            <form method="post" onSubmit={onSubmit}>
-              <input value={csrfToken} id="csrf_token" type="hidden" />
-              <input
-                ref={emailRef}
-                id="email"
-                name="email"
-                type="text"
-                className="form-control"
-                placeholder="Enter your email address"
-              />
-              <label htmlFor="email"> Email </label>
-              <div className="col-8 ">
-                <a
-                  href="#"
-                  className={`${styles.button} btn btn-red w-100 py-1`}
-                >
-                  <button type="submit"> Reset Password</button>
-                </a>
-                {isSent ? <span><h4>Recovery Email Sent</h4></span> : null}
-              </div>
-            </form>
-          </div>
+    <main>
+      <section className="wrapper bg-light">
+        <div className="container pb-14 pb-md-16">
+          <div className="row">
+            <div className="col-lg-7 col-xl-6 col-xxl-5 mx-auto">
+              <div className="col-3 my-10 ps-3">
+                <Image
+                  className={`${styles.company_logo}`}
+                  src={companyLogo}
+                  alt="image"
+                  />
+            </div>
 
-          <Link className={styles.a} href="/auth/login">
-            <p
-              className={`${styles.return_text} col-8 d-flex ms-0 justify-content-center px-0`}
-            >
-              Return to Sign-In
-            </p>
-          </Link>
+            <div className="card">
+              <div className="card-body p-11">
+                <div className="d-flex flex-column align-items-center ms-0 px-0">
+                  <h4 className="col-12 px-0 ms-0">
+                    <b>Reset Your Password</b>
+                    <p className={`${styles.header_text} pt-2 mb-4 col-12`}>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe at
+                      veniam autem soluta reiciendis repellat.
+                    </p>
+                  </h4>
+                  <div className=" mb-4 col-12 px-0">
+                      <form method="post" onSubmit={onSubmit}>
+                        <div className="form-floating">
+                        <input value={csrfToken} id="csrf_token" type="hidden" />
+                        <input
+                          ref={emailRef}
+                          id="email"
+                          name="email"
+                          type="text"
+                          className="form-control"
+                          placeholder="Enter your email address"
+                        />
+                        <label htmlFor="email"> Email </label>
+                        </div>
+
+                        <div className="col-12">
+                          <a href="#">
+                              <button type="submit" className={`${styles.button} btn btn-red w-100 py-1`}> Reset Password</button>
+                            </a>
+                            {isSent ? <span><h4>Recovery Email Sent</h4></span> : null}
+                        </div>
+                      </form>
+                  </div>
+
+                    <Link className={styles.a} href="/auth/login">
+                      <p className={`${styles.return_text} col-8 d-flex ms-0 my-0 justify-content-center px-0`}>
+                        Return to Sign-In
+                      </p>
+                    </Link>
+                  </div>
+              </div>
+            </div>
+
+
+            </div>
+          </div>
         </div>
-      </div>
-      <div
-        className={`${styles.contact_info} d-flex col-3 offset-1 mt-9 justify-content-between`}
-      >
-        <p>(c) arcton</p>
-        <a href="arcton.com">
-          <p>Contact</p>
-        </a>
-        <a href="arcton.com">
-          <p>Privacy & Terms</p>
-        </a>
-      </div>
-    </div>
+      </section>
+    </main>
   );
 };
 
